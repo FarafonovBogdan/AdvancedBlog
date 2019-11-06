8864bba19b181d544129db59bec5df7cbc9ab916
FarafonovBogdan <bogdanf.dev@gmail.com>
2019-11-06 13:06:41 +0200

    Update

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from .models import Comment,Contact
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'body')
